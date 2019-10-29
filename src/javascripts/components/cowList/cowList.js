850ade3a8ed6365ea223ea8c650c957da439c883
Zoe Ames <zoeames@gmail.com>
2019-10-28 19:00:46 -0500

    cows are an array of objects

--- src/javascripts/components/cowList/cowList.js
+++ src/javascripts/components/cowList/cowList.js
@@ -1,11 +1,19 @@
-import cows from '../../helpers/data/cows';
+import cowData from '../../helpers/data/cows';
 
 import './cowList.scss';
 
 const buildCows = () => {
-  cows.getCows()
+  cowData.getCows()
     .then((response) => {
-      console.log('it worked!', response.data);
+      console.log(response);
+      const demCows = response.data.cows;
+      const cows = [];
+      Object.keys(demCows).forEach((cowId) => {
+        // demCows['cow3']
+        demCows[cowId].id = cowId;
+        cows.push(demCows[cowId]);
+      });
+      console.log('it worked!', cows);
     })
     .catch((error) => {
       console.error('shit broke', error);
